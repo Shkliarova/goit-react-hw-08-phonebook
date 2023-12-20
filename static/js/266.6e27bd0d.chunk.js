f33735f5d8fb4b0d53902086e5065fc622c44e41
Shkliarova <130659546+Shkliarova@users.noreply.github.com>
2023-12-20 16:50:59 +0000

    Deploying to gh-pages from @ Shkliarova/goit-react-hw-08-phonebook@7a61a6946a42e429d8d05dceeb144a92a5940fe1 🚀

---
+++ static/js/266.6e27bd0d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{1266:function(e,t,n){n.r(t),n.d(t,{default:function(){return r}});var o=n(184);function r(){return(0,o.jsxs)("div",{style:{textAlign:"center",marginTop:"15%"},children:[(0,o.jsx)("h1",{children:"Welcome to phonebook!"}),(0,o.jsx)("p",{style:{fontSize:"larger"},children:"To access your contact book, please register or sign in to your existing account."})]})}}}]);
+//# sourceMappingURL=266.6e27bd0d.chunk.js.map
--- static/js/266.a6edfc14.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{1266:function(e,t,n){n.r(t),n.d(t,{default:function(){return o}});var r=n(184);function o(){return(0,r.jsx)("div",{children:(0,r.jsx)("h1",{children:"Welcome to phonebook!"})})}}}]);
-//# sourceMappingURL=266.a6edfc14.chunk.js.map
